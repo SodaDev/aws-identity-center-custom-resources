cbdc3960e0a246894deb7a68fa3ab566c512ee0a
Marcin Sodkiewicz <sodkiewicz.m@gmail.com>
2022-10-07 12:50:25 +0200

    init

---
+++ identity-store/user/tests/unit/test-handler.test.ts
@@ -0,0 +1,8 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { lambdaHandler } from '../../app';
+
+describe('Unit test for app handler', function () {
+    it.skip('verifies successful response', async () => {
+
+    });
+});
